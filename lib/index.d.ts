e080aebb051fe4dbd4d940648d935b216bc60c07
Bennett Wu <57691028+bwu1324@users.noreply.github.com>
2022-12-16 21:51:58 -0600

    feat(downloader): add downloader function to allow for abortable downloads

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,8 +1,16 @@
 /// <reference types="node" />
 import { PassThrough } from 'stream';
+declare class Downloader {
+    private downloader_;
+    private output_;
+    get stream(): PassThrough;
+    constructor(url: string, args: Array<string>);
+    Abort(): void;
+}
 declare const ytdlp: {
     path: string;
     version: string;
+    downloader: (url: string, args: string[]) => Downloader;
     stream: (url: string, args: string[]) => PassThrough;
 };
 export default ytdlp;
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,56 @@
 import fs from 'fs';
 import path from 'path';
-import { spawn } from 'child_process';
+import { ChildProcessWithoutNullStreams, spawn } from 'child_process';
 import { PassThrough } from 'stream';
+
+class Downloader {
+	private downloader_: ChildProcessWithoutNullStreams;
+	private output_: PassThrough;
+
+	get stream() { return this.output_; }
+
+	constructor(url: string, args: Array<string>) {
+		args.splice(0, 0, url);
+		args.push(...['-o', '-']);
+		args.push('--no-part');
+		args.push('--no-playlist');
+		args.push('--quiet',);
+		this.downloader_ = spawn(ytdlp.path, args);
+
+		this.output_ = new PassThrough();
+		this.downloader_.stdout.pipe(this.output_);
+	}
+
+	Abort() {
+		this.downloader_.kill('SIGINT');
+	}
+}
 
 const meta = JSON.parse(fs.readFileSync(path.join(__dirname, 'meta.json'), 'utf-8'));
 
-const ytdlp: { path: string, version: string, stream: (url: string, args: string[]) => PassThrough } = {
+const ytdlp: {
+	path: string,
+	version: string,
+	downloader: (url: string, args: string[]) => Downloader,
+	stream: (url: string, args: string[]) => PassThrough
+} = {
 	path: meta.path,
 	version: meta.version,
-	stream: undefined as any
-};
-ytdlp.stream = (url, args) => {
-	args.splice(0, 0, url);
-	args.push(...['-o', '-']);
-	args.push('--no-part');
-	args.push('--no-playlist');
-	args.push('--quiet',);
-	const downloader = spawn(ytdlp.path, args);
+	downloader: (url, args) => {
+		return new Downloader(url, args);
+	},
+	stream: (url, args) => {
+		args.splice(0, 0, url);
+		args.push(...['-o', '-']);
+		args.push('--no-part');
+		args.push('--no-playlist');
+		args.push('--quiet',);
+		const downloader = spawn(ytdlp.path, args);
 
-	const output = new PassThrough();
-	downloader.stdout.pipe(output);
-	return output;
+		const output = new PassThrough();
+		downloader.stdout.pipe(output);
+		return output;
+	}
 };
 
 export default ytdlp;
